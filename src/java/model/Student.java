da95a7c6b63c7f38e94487167f203ddf2d35a0a7
BCMPF <BCMPF@gmail.com>
2021-12-01 23:27:58 -0500

    first commit

---
+++ src/java/model/Student.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author BeatrizFortes
+ */
+public class Student {
+    String id;
+    String firstName;
+    String lastName;
+    String gender;
+    
+    public Student() {}
+
+    public Student(String id, String firstName, String lastName, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+    
+}
